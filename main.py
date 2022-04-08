b3e95733cd24d87133f4897869d43fdb2da1ddf5
ngrpv <nazar.garipov475@gmail.com>
2022-04-08 16:35:14 +0500

    Add readme.md and errors handling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 import socket
-
 import requests
 
 
@@ -8,41 +7,47 @@ def trace(ip, hops):
     try:
         dest = socket.gethostbyname(ip)
     except socket.error:
-        log('Invalid host name')
+        log("Can't resolve ip address for {}. Check internet connection".format(ip))
         return
     log('tracing: {}'.format(dest))
     ttl = 0
     port = 33434
-    while ttl < hops:
-        ttl += 1
-        receiver = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-        receiver.bind(('', port))
-        receiver.settimeout(2)
-        sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.getprotobyname('udp'))
-        sender.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-        sender.sendto(b'', (dest, port))
-        answer_from = None
-        try:
-            _, answer_from = receiver.recvfrom(65536)
-        except socket.error:
-            pass
-        finally:
-            receiver.close()
-            sender.close()
-        if answer_from:
-            as_num = get_info(answer_from[0])
-            log('{}\t{}\t{}'.format(ttl, answer_from[0], as_num))
-        else:
-            log('{}\t *****'.format(ttl))
-            continue
-        if answer_from[0] == dest:
-            break
+    try:
+        while ttl < hops:
+            ttl += 1
+            receiver = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+            receiver.bind(('', port))
+            receiver.settimeout(2)
+            sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.getprotobyname('udp'))
+            sender.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+            sender.sendto(b'1234', (dest, port))
+            answer_from = None
+            try:
+                _, answer_from = receiver.recvfrom(65536)
+            except socket.error:
+                pass
+            finally:
+                receiver.close()
+                sender.close()
+            if answer_from:
+                as_num = get_info(answer_from[0])
+                log('{:<3}\t{:<20}\t{:<25}'.format(ttl, answer_from[0], as_num))
+            else:
+                log('{}\t *****'.format(ttl))
+                continue
+            if answer_from[0] == dest:
+                break
+    except Exception as e:
+        print('Something went wrong: {}'.format(e))
 
 
 def get_info(ip):
-    info = json.loads(
-        requests.get("http://ipinfo.io/{0}/json".format(ip)).content)
-    return info.get('org', '__LOCAL__') + '\t' + info.get('country', '')
+    try:
+        info = json.loads(
+            requests.get("http://ipinfo.io/{0}/json".format(ip)).content)
+        return '{:<30}{:<5}'.format(info.get('org', '__LOCAL__'), info.get('country', ''))
+    except Exception:
+        return "*****"
 
 
 def log(str):
@@ -51,4 +56,4 @@ def log(str):
 
 print('ip or domain:')
 ip = input()
-trace(ip, 45)
+trace(ip, 30)
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Autonomous System Tracer (+country, provider)
+## Usage:
+1. Linux(on Windows may be troubles with ttl):
+   ```
+   [user@user]$ sudo python main.py
+    ip or domain:
+    google.com
+    tracing: 108.177.14.100
+    1       192.168.1.1             __LOCAL__        
+   ...     
+    
+ ![](Screenshot_20220408_162229.png)
